da33bf184ca0bdb30844f3116f572dd137b37751
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-10-01 11:58:43 +0530

    EF Migrations created

---
+++ LibrarayManagementSystem/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibrarayManagementSystem.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=127.0.0.1;database=libraray_management_sys;uid=root;pwd=");
+        }
+    }
+}
+
---
+++ LibrarayManagementSystem/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibrarayManagementSystem.Models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+        public String Name { get; set; }
+    }
+}
+
