ae9e4b7ec48d16eed77a3444dbb61504041a46c2
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-11-01 09:31:18 +0530

    Switched to VS Code and freshly created the project using dotnet core commandline

--- LibrarayManagementSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LibrarayManagementSystem.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
