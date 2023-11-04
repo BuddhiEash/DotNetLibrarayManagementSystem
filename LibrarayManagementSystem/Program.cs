d30a9c3abcd2717c7cdcb606c5941b2d25743bef
Don Buddhi Eashwarage <buddhieash@gmail.com>
2023-11-04 17:51:48 +0530

    DB context related code updated

--- LibrarayManagementSystem/Program.cs
+++ LibrarayManagementSystem/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var connectionString = builder.Configuration.GetConnectionString("AppDbConnectionString");
-builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, 
+ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
