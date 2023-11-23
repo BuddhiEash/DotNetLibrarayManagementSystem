87117d177bc7ae459d34129289afcbe2b177d129
Don Buddhi <buddhieash@gmail.com>
2023-11-23 15:04:00 +0530

    Updated UserRole.cs

--- LibrarayManagementSystem/Models/UserRole.cs
+++ LibrarayManagementSystem/Models/UserRole.cs
@@ -3,7 +3,7 @@ namespace LibrarayManagementSystem.Models;
 public class UserRole
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int IsActive { get; set; }
     public DateTime TimeStamps { get; set; }
 }
