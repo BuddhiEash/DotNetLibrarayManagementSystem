03014dbe9611846698c06feefdf303f64c53f474
Don Buddhi <buddhieash@gmail.com>
2023-11-24 21:11:54 +0530

    Updated UserRoleController

--- LibrarayManagementSystem/Controllers/UserRoleController.cs
+++ LibrarayManagementSystem/Controllers/UserRoleController.cs
@@ -1,44 +1,29 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using LibrarayManagementSystem.Models;
-using LibrarayManagementSystem.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using LibrarayManagementSystem.Data.Interfaces;
 
 namespace LibrarayManagementSystem.Controllers;
 
 public class UserRoleController : Controller
 {
-    private readonly AppDbContext _appDbContext;
+    private readonly IUserRolesRepository _userRolesRepository;
     private readonly ILogger<UserRoleController> _logger;
-    public UserRoleController(AppDbContext appDbContext, ILogger<UserRoleController> logger)
+    public UserRoleController(IUserRolesRepository userRolesRepository, ILogger<UserRoleController> logger)
     {
-        _appDbContext = appDbContext;
+        _userRolesRepository = userRolesRepository;
         _logger = logger;
     }
 
     [HttpGet]
     public IActionResult Index()
     {
-        var userRoles = GetRoles();
+        var customers = _userRolesRepository.GetAll();
         ViewData["roles"] = null;
         _logger.LogInformation("Loading User Controller Index View");
-        if (userRoles is not null) 
+        if (customers is not null) 
         {
             _logger.LogInformation($"User roles are not null");
-            ViewData["roles"] = userRoles;
+            ViewData["roles"] = customers;
         }
         return View();
-    }
-
-    public List<UserRole>? GetRoles()
-    {
-        try
-        {
-            return _appDbContext.UserRoles.OrderBy(u => u.Id).Take(10).ToList();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogInformation($"Failed to get the latest user:{ex}");
-            return null;
-        }
     }
 }
